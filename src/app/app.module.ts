f6ce09367197e61c495a99be95a80f4c7b6cff9b
patrickNgare <patrickngare@gmail.com>
2019-01-11 14:38:44 +0300

    deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { AboutComponent } from './about/about.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CustomersComponent } from './customers/customers.component';
+import { PaymentComponent } from './payment/payment.component';
+const routes:Routes=[
+  {path:"customers",component:CustomersComponent},
+  {path:"about",component:AboutComponent},
+  {path:"orders",component:OrdersComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    OrdersComponent,
+    CustomersComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
